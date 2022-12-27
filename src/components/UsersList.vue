6a7f56b075fba27eda48bf25c3f5288ecc3c71c3
stu96534 <stu96534@gmail.com>
2022-12-27 22:06:27 +0800

    feat: add view UserList

---
+++ src/components/UsersList.vue
@@ -0,0 +1,126 @@
+<template>
+  <ul class="list-group">
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      <div class="row align-items-center">
+        <h3 class="col">An item</h3>
+        <h6 class="col mt-2">user1@example.com</h6>
+      </div>
+    
+      <div>
+        <button type="button" class="btn btn-secondary me-2">Secondary</button>
+        <button type="button" class="btn btn-secondary">Secondary</button>
+      </div>
+    </li>
+
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      <div class="row align-items-center">
+        <h3 class="col">An item</h3>
+        <h6 class="col mt-2">user1@example.com</h6>
+      </div>
+    
+      <div>
+        <button type="button" class="btn btn-secondary me-2">Secondary</button>
+        <button type="button" class="btn btn-secondary">Secondary</button>
+      </div>
+    </li>
+
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      <div class="row align-items-center">
+        <h3 class="col">An item</h3>
+        <h6 class="col mt-2">user1@example.com</h6>
+      </div>
+    
+      <div>
+        <button type="button" class="btn btn-secondary me-2">Secondary</button>
+        <button type="button" class="btn btn-secondary">Secondary</button>
+      </div>
+    </li>
+
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      <div class="row align-items-center">
+        <h3 class="col">An item</h3>
+        <h6 class="col mt-2">user1@example.com</h6>
+      </div>
+    
+      <div>
+        <button type="button" class="btn btn-secondary me-2">Secondary</button>
+        <button type="button" class="btn btn-secondary">Secondary</button>
+      </div>
+    </li>
+
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      <div class="row align-items-center">
+        <h3 class="col">An item</h3>
+        <h6 class="col mt-2">user1@example.com</h6>
+      </div>
+    
+      <div>
+        <button type="button" class="btn btn-secondary me-2">Secondary</button>
+        <button type="button" class="btn btn-secondary">Secondary</button>
+      </div>
+    </li>
+
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      <div class="row align-items-center">
+        <h3 class="col">An item</h3>
+        <h6 class="col mt-2">user1@example.com</h6>
+      </div>
+    
+      <div>
+        <button type="button" class="btn btn-secondary me-2">Secondary</button>
+        <button type="button" class="btn btn-secondary">Secondary</button>
+      </div>
+    </li>
+
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      <div class="row align-items-center">
+        <h3 class="col">An item</h3>
+        <h6 class="col mt-2">user1@example.com</h6>
+      </div>
+    
+      <div>
+        <button type="button" class="btn btn-secondary me-2">Secondary</button>
+        <button type="button" class="btn btn-secondary">Secondary</button>
+      </div>
+    </li>
+
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      <div class="row align-items-center">
+        <h3 class="col">An item</h3>
+        <h6 class="col mt-2">user1@example.com</h6>
+      </div>
+    
+      <div>
+        <button type="button" class="btn btn-secondary me-2">Secondary</button>
+        <button type="button" class="btn btn-secondary">Secondary</button>
+      </div>
+    </li>
+
+
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      <div class="row align-items-center">
+        <h3 class="col">An item</h3>
+        <h6 class="col mt-2">user1@example.com</h6>
+      </div>
+    
+      <div>
+        <button type="button" class="btn btn-secondary me-2">Secondary</button>
+        <button type="button" class="btn btn-secondary">Secondary</button>
+      </div>
+    </li>
+
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      <div class="row align-items-center">
+        <h3 class="col">An item</h3>
+        <h6 class="col mt-2">user1@example.com</h6>
+      </div>
+    
+      <div>
+        <button type="button" class="btn btn-secondary me-2">Secondary</button>
+        <button type="button" class="btn btn-secondary">Secondary</button>
+      </div>
+    </li>
+
+   
+  </ul>
+</template>
--- src/views/AdminPage.vue
+++ src/views/AdminPage.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="container py-5">
-    <div class="column mt-4">
+    <div class="column mt-3">
       <button type="button" class="btn btn-success btn-lg">新增員工</button>
     </div>
 
-    <div class="column">
-      <!-- UsersCard -->
+    <div class="column mt-4">
+      <UsersList />
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import UsersList from "../components/UsersList.vue"
+
+</script>
