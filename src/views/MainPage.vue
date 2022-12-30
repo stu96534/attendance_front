d299bbe6578fbfdcdb586b26e923e4ce39225673
stu96534 <stu96534@gmail.com>
2022-12-30 16:43:56 +0800

    feat: add currentUser data in attendant view

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -3,11 +3,11 @@
     <div class="card card-body mx-auto d-flex">
         
         <div class="text-center">
-          <img src="https://fakeimg.pl/120x130" class="rounded mt-5" alt="...">
+          <img :src= "image"  class="rounded mt-5" alt="...">
         </div>
 
         <div class="text-center mb-4">
-          <h1 class="h3 mb-2 mt-4 font-weight-normal fs-1">Name</h1>
+          <h1 class="h3 mb-2 mt-4 font-weight-normal fs-1">{{ name }}</h1>
         </div>
 
         <div class="text-center mb-4">
@@ -32,3 +32,25 @@
  height: 110px;
 }
 </style>
+
+<script setup lang="ts">
+import { ref, computed } from "vue";
+import usersAPI from "../apis/users";
+import { Toast } from "../utils/helpers"
+import { useRouter } from "vue-router";
+import { useStore } from "vuex";
+
+const router = useRouter();
+const store = useStore();
+const currentUser = computed(() => store.getters.currentUser);
+const name = ref(currentUser.value.name
+);
+const image = ref(currentUser.value.image
+);
+
+if(!image.value) {
+  image.value = 'https://fakeimg.pl/120x130'
+}
+
+
+</script>
