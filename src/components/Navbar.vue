30462ece342074f2dff036fb1df4d4c03d7a66c1
stu96534 <stu96534@gmail.com>
2023-01-05 22:31:37 +0800

    modify title

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <RouterLink to="/" class="navbar-brand">鈦鉭出勤</RouterLink>
+      <RouterLink to="/" class="navbar-brand">鈦坦出勤</RouterLink>
 
       <button
         class="navbar-toggler"
