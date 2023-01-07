2a0850a53f0a54004a95c706b3b3c8197d19da37
stu96534 <stu96534@gmail.com>
2023-01-08 04:39:33 +0800

    modify rebuild

---
+++ dist/assets/attendant-b50d7ede.js
@@ -0,0 +1,1 @@
+import{a as t}from"./index-ad6355ca.js";const o={addDate({userId:a,date:e}){return t.put(`/attendant/${a}`,{date:e})},getLocation(){return t.get("/attendant/location")}};export{o as a};
